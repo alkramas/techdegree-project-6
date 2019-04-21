2713551e6b3011f5bfe07f0fc9de387893264a91
Fabian Kramer <kramas@gmx.ch>
2019-04-21 23:44:14 +0200

    project files and basic varaibles and functions

---
+++ js/app.js
@@ -0,0 +1,44 @@
+
+// dom element selection
+const qwerty = document.querySelector('div#qwerty');
+const phrase = document.querySelector('div#phrase');
+let phraseArray;
+let missed = 0;
+
+// array of phrases
+const phrases = ['I still havnt found what I am looking for', 'Stuck in a moment', 'City of blinding lights', 'Joshua Tree', 'Sometimes You Cant Make It On Your Own'];
+
+// select overlay element and hide on click
+const overlay = document.querySelector('div#overlay');
+document.querySelector('a.btn__reset').addEventListener('click', function() {
+  overlay.style.display = 'none';
+});
+
+// get a random phrase and split array item into characters
+function getRandomPhraseAsArray(arr) {
+  let randomNumber = Math.floor((Math.random() * 5) + 0);
+  // console.log(randomNumber);
+  let phrase = arr[randomNumber];
+  // console.log(phrase);
+  phraseArray = phrase.split("");
+  console.log(phraseArray);
+};
+// call ramdom phrase function returns array of characters
+getRandomPhraseAsArray(phrases);
+
+
+function addPhraseToDisplay(arr) {
+  let x;
+  for (x in phraseArray) {
+    let li = document.createElement('li');
+    li.innerText = phraseArray[x];
+    let ul = phrase.children;
+      if (phraseArray[x] !== " ") {
+          li.classList.add("letter");
+         }
+    ul[0].appendChild(li);
+    console.log(phrase);
+  };
+};
+
+addPhraseToDisplay(phraseArray);
