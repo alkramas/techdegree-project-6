8f83b146348effc59d948e8b91ebc39165624d86
Fabian Kramer <fabian.kramer@ionesoft.ch>
2019-04-26 10:01:40 +0200

    update js: looping through all letters, removing hearts on failed attempts

--- js/app.js
+++ js/app.js
@@ -2,12 +2,14 @@
 // dom element selection
 const qwerty = document.querySelector('div#qwerty');
 const phrase = document.querySelector('div#phrase');
+const scoreboard = document.querySelector('ol');
+
 // let typedletter;
 let phraseArray;
 let missed = 0;
 
 // array of phrases
-const phrases = ['I still havnt found what I am looking for', 'Stuck in a moment', 'City of blinding lights', 'Joshua Tree', 'Sometimes You Cant Make It On Your Own'];
+const phrases = ['I still havnt found what I am looking for', 'Vertigo', 'Man and Woman', 'Stuck in a moment', 'City of blinding lights', 'Joshua Tree', 'Sometimes You Cant Make It On Your Own'];
 
 // select overlay element and hide on click
 const overlay = document.querySelector('div#overlay');
@@ -17,10 +19,8 @@ document.querySelector('a.btn__reset').addEventListener('click', function() {
 
 // get a random phrase and split array item into characters
 function getRandomPhraseAsArray(arr) {
-  let randomNumber = Math.floor((Math.random() * 5) + 0);
-  // console.log(randomNumber);
+  let randomNumber = Math.floor((Math.random() * 7) + 0);
   let phrase = arr[randomNumber];
-  // console.log(phrase);
   phraseArray = phrase.split("");
   console.log(phraseArray);
 };
@@ -38,7 +38,6 @@ function addPhraseToDisplay(arr) {
           li.className = "letter";
          }
     ul[0].appendChild(li);
-    // console.log(phrase);
   };
 };
 addPhraseToDisplay(phraseArray);
@@ -51,23 +50,43 @@ window.setTimeout((something) => {
 }, 0, 'timeout logged');
 
 
+// function: test typed letter with phrase, handle branches
 function checkLetter(button) {
   // get selection of all li letters content for comparison
-  let phraseletters = document.querySelectorAll('.letter');
+  const phraseletters = document.querySelectorAll('.letter');
   const letterFound = button.innerText;
-  const phraseArrayString = phraseArray.toString();
+  const phraseString = phraseArray.toString();
+  const phraseStringLower = phraseString.toLowerCase();
 
-  // console.log(letterFound);
-  for (let i = 0; i <   phraseletters.length; i += 1) {
-    let phraselettersTextValue = phraseletters[i].innerText;
-    let phraselettersText = phraselettersTextValue.toLowerCase();
-    // console.log(phraselettersText);
-    if (phraselettersText.includes(letterFound)) {
-      // phraselettersText[i].parentNode.addClass('show');
-      console.log('found match');
-      phraseletters[i].classList.add('show');
-    }
+  // handle wrong guesses
+  if (!phraseStringLower.includes(letterFound)) {
+    missed += 1;
+    const scoreboardItem = document.querySelector('.tries');
+    console.log('you have ' + (5 - missed) + ' guess(es) left');
+    scoreboard.removeChild(scoreboardItem);
+  } else {
+      // handle correct guesses
+      for (let i = 0; i < phraseletters.length; i += 1) {
+        let phraselettersItem = phraseletters[i];
+        let phraselettersItemHTML = phraselettersItem.innerHTML;
+        let phraselettersText = phraselettersItemHTML.toLowerCase();
+
+        if (phraselettersText.includes(letterFound)) {
+          console.log('found match with letter ' + letterFound);
+          phraseletters[i].classList.add('show');
+        }
+      }
   }
+};
+
+function checkWin() {
+
+  if () {
+
+  } else if () {
+
+  }
+  console.log();
 };
 
 // Add an event listener to the keyboard container, call checkLetter function on event
@@ -77,7 +96,9 @@ qwerty.addEventListener('click', (event) => {
       event.target.className = 'chosen';
       event.target.disabled = true;
       const targetbutton = event.target;
-      // console.log(targetbutton);
+
       checkLetter(targetbutton);
+
+      //checkWin();
   }
 });
